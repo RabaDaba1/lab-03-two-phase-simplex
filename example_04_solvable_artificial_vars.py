6399936408dc1ef647f73b501cbe777649b38bf7
Kacper Rabczewski <krabczewski@gmail.com>
2023-04-11 11:22:10 +0000

    Update file example_04_solvable_artificial_vars.py

--- example_04_solvable_artificial_vars.py
+++ example_04_solvable_artificial_vars.py
@@ -9,6 +9,15 @@ def create_model() -> Model:
     # fill missing test based on the example_01_solvable.py
     # to make the test a bit more interesting:
     # * make sure solver has to use some artificial variables
+    x1 = model.create_variable("x1")
+    x2 = model.create_variable("x2")
+
+    model.add_constraint(x1 + 2*x2 <= 20)
+    model.add_constraint(x1 + x2 == 10)
+
+    model.minimize(2*x1 + x2)
+
+
     return model
 
 def run():
@@ -17,8 +26,16 @@ def run():
     # add a test "assert something" based on the example_01_solvable.py
     # TIP: you may use other solvers (e.g. https://online-optimizer.appspot.com)
     #      to find the correct solution
-    logging.info("This test is empty but it shouldn't be, fix it!")
-    raise AssertionError("Test is empty")
+    try:
+        solution = model.solve()
+    except:
+        raise AssertionError("This problem has a solution and your algorithm hasn't found it!")
+
+    logging.info(solution)
+
+    assert (solution.assignment(model) == [10, 0.0]), "Your algorithm found an incorrect solution!"
+
+    logging.info("Congratulations! This solution seems to be alright :)")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(message)s')
