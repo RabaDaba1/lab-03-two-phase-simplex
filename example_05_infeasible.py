230847915e5abf3cdb25efe8d1503b77373b36f0
Kacper Rabczewski <krabczewski@gmail.com>
2023-04-11 13:33:11 +0000

    Update file example_05_infeasible.py

--- example_05_infeasible.py
+++ example_05_infeasible.py
@@ -11,12 +11,12 @@ def create_model() -> Model:
     # * make sure model is infeasible
     x1 = model.create_variable("x1")
     x2 = model.create_variable("x2")
-    x3 = model.create_variable("x3")
 
-    model.add_constraint(x1 - 3*x2 + x3 >= 10)
-    model.add_constraint(x1 + 2*x2 - x3 <= -10)
+    model.add_constraint(2*x1 >= 1200)
+    model.add_constraint(x1 + x2 <= 400)
+    mode.add_constraint(2*x1+3/2*x2 >= 900)
 
-    model.maximize(5 * x1 + 8 * x2)
+    model.maximize(-2*x1 - 3*x2)
     return model
 
 def run():
